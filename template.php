67bc92fcaaccd765b2dec474ab55f4434e2e4338
Kasper Garnæs <kasper@reload.dk>
2010-06-07 16:10:45 +0200

    Replaced search examples with link to advanced OPAC search page. #1344.

--- template.php
+++ template.php
@@ -24,12 +24,15 @@ function copenhagen_user_login_block($form){
 }
 
 /**
- * Override from base theme to switch background image
+ * Override from base theme to switch background image and set example text
  */
 function copenhagen_ting_search_form($form){
 	$form['submit']['#type'] 	= "image_button" ;
 	$form['submit']['#src'] 	= drupal_get_path('theme','copenhagen')."/images/searchbutton.png";
 	$form['submit']['#attributes']['class'] 	= "";
+	
+	
+	$form['example_text']['#value'] = '<div class="example"><p>'.l(t('Feltsøgning'), 'https://biblioteksbase.kk.dk/sites/XWW/pub/search.html?doaction=&data=advanced=true', array('absolute' => true)).'</p></div>';
 
 	return drupal_render($form);	
 }
