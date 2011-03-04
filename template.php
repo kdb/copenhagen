677edfa3be602b0e2f502e91294d32d241902179
Mikkel Hoegh <mikkel@hoegh.org>
2011-03-04 10:32:48 +0100

    Whitespace, formatting.

--- template.php
+++ template.php
@@ -1,8 +1,8 @@
 <?php
-/* =====================================
-  Copenhagen
-  template.php
-* ------------------------------------- */
+/**
+ * @file
+ * Template code for the Copenhagen theme.
+ */
 
 /* forms */
 
@@ -15,11 +15,11 @@ function copenhagen_user_login_block($form){
 	$form['submit']['#attributes']['class'] 	= "";
 
 
-	$name = drupal_render($form['name']); 
-	$pass =  	drupal_render($form['pass']); 
-	$submit =  	drupal_render($form['submit']); 
-	$remember =	drupal_render($form['remember_me']); 
-	
+	$name = drupal_render($form['name']);
+	$pass = drupal_render($form['pass']);
+	$submit = drupal_render($form['submit']);
+	$remember =	drupal_render($form['remember_me']);
+
 	return 	$name . $pass .$submit . $remember . drupal_render($form);
 }
 
@@ -27,35 +27,36 @@ function copenhagen_user_login_block($form){
  * Override from base theme to switch background image and set example text
  */
 function copenhagen_ting_search_form($form){
-	$form['submit']['#type'] 	= "image_button" ;
-	$form['submit']['#src'] 	= drupal_get_path('theme','copenhagen')."/images/searchbutton.png";
-	$form['submit']['#attributes']['class'] 	= "";
-	
-	
-	$form['example_text']['#value'] = '<div class="example">'. theme('item_list', array(
+	$form['submit']['#type'] = "image_button" ;
+	$form['submit']['#src']	= drupal_get_path('theme','copenhagen') . "/images/searchbutton.png";
+	$form['submit']['#attributes']['class'] = "";
+
+
+	$form['example_text']['#value'] = '<div class="example">' . theme('item_list', array(
       l(t('Feltsøgning'), 'https://biblioteksbase.kk.dk/sites/XWW/pub/search.html?doaction=&data=advanced=true', array('absolute' => true)),
       l(t('Sådan bruger du hjemmesiden'), 'vejledning'),
-    )).'</div>';
- 
-	return drupal_render($form);	
+    )) . '</div>';
+
+	return drupal_render($form);
 }
 
 /**
  * Override from module to collapse groups of loans which are not overdue into one.
  */
 function copenhagen_ding_library_user_loan_list_form($form) {
-  //Make sure we have somewhere to place our loans
+  // Make sure we have somewhere to place our loans
 	if (!isset($form['loan_data']['#grouped']['due'])) {
     $form['loan_data']['#grouped']['due'] = array();
   }
 	foreach ($form['loan_data']['#grouped'] as $date => $group) {
 		if (!in_array($date, array('overdue', 'due'))) {
-			//Merge date group contents into due groupe
+			// Merge date group contents into due groupe
 			$form['loan_data']['#grouped']['due'] += $group;
-			
-			//Remove date group
+
+			// Remove date group
 			unset($form['loan_data']['#grouped'][$date]);
 		}
   }
   return theme_ding_library_user_loan_list_form($form);
 }
+
