7e7a85344f4402cbc3947be2129d30f30b743991
Mikkel Hoegh <mikkel@hoegh.org>
2012-04-18 13:50:33 +0200

    Removed obsolete admin module 1.x stuff.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -16,11 +16,7 @@
   <?php print $styles; ?>
   <?php print $scripts; ?>
 </head>
-<body class="<?php print $body_classes; ?><?php if (!empty($admin)) print ' '.admin;  ?>">
-<?php 
-/*adds support for for the admin module*/
-  if (!empty($admin)) print $admin; 
-?>
+<body class="<?php print $body_classes; ?>">
 
 <?php if ($help OR $messages) { ?>
   <div id="drupal-messages">
