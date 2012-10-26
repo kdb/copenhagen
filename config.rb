3d7bfeec3d0473afece100838d75341ed1038ae9
Mikkel Hoegh <mikkel@hoegh.org>
2012-10-26 13:48:08 +0200

    Added CSS3-based buttons.
    * Getting rid of the the old image-based/sliding doors styling.
    * Using SASS for the button stylesheet.

---
+++ config.rb
@@ -0,0 +1,10 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+fonts_dir = "fonts"
+images_dir = "images"
+javascripts_dir = "javascripts"
+output_style = :compressed
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
